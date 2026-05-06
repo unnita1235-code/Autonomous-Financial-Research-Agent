4a4f6012374512f0b4100d30cc3140c5ad11472f
unnita1235 <unnita1235@gmail.com>
2026-05-06 19:24:58 +0530

    Project complete - full pipeline working

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning={true}>
       <body
         className={`${outfit.variable} ${jetbrainsMono.variable} min-h-screen font-sans antialiased flex flex-col`}
       >
